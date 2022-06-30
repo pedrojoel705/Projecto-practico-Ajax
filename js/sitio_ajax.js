b935d736fb539fecae3834b75fed8bd6dbe5539e
pedrojoel705 <pedrojoel705@gmail.com>
2022-06-30 04:48:07 -0300

    segundo commit

---
+++ js/sitio_ajax.js
@@ -0,0 +1,29 @@
+const $main = document.querySelector('main');
+
+const getHTML = async () => {
+  try {
+    let res = await axios.get('assets/home.html');
+    let json = await res.data;
+    console.log(json, res);
+
+    $main.innerHTML = json;
+  } catch (err) {
+    let message = err.message || 'Ocurrió un error';
+    $main.insertAdjacentHTML('afterend', `Error ${err.status}: ${message}`);
+  }
+};
+document.addEventListener('click', async (e) => {
+  if (e.target.matches('.menu a')) {
+    e.preventDefault();
+    try {
+      res = await axios(e.target.href);
+      json = res.data;
+      $main.innerHTML = json;
+    } catch (err) {
+      let message = err.message || 'Ocurrió un error';
+      $main.insertAdjacentHTML('afterend', `Error ${err.status}: ${message}`);
+    }
+  }
+});
+
+document.addEventListener('DOMContentLoaded', getHTML);
